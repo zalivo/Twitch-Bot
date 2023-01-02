5c5ed32f1523ffa06759751a8d6d1a8d50f79dc6
zalivo <izaloudek5@gmail.com>
2023-01-02 15:34:53 +0100

    README.md slight edit main.py check if bot connected, listen to messages, check that you can send links

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Twitch Bot<br>
 cc
 ## List of (soon) functional commands
+- [x] Printout random String from list
+- [x] Simple command
 - Count
 - Chat interaction
-- Prinout random String from list
-- Printout random String from list
 - Printout random number from certain range
 - ASCII image?
--- main.py
+++ main.py
@@ -8,6 +8,16 @@ class Bot(commands.Bot):
     def __init__(self):
         super().__init__(token=os.environ['TWITCH_TOKEN'], prefix='?', initial_channels=['Riccc0_'])
 
+    async def event_ready(self):
+        print(f'Logged in as | {self.nick}')
+        print(f'User id is | {self.user_id}')
+
+    async def event_message(self, message):
+        if message.echo:
+            return
+        print(message.content)
+        await self.handle_commands(message)
+
     # Function: When someone from chat types ?hello
     # Bot will respond with Hello message back to the chatter
     @commands.command()
@@ -17,10 +27,16 @@ class Bot(commands.Bot):
     # Function: printout random strings from array
     # TODO
     # properly rename command nevim
+    # fill list with Strings
     @commands.command()
     async def nevim(self, ctx:commands.Context):
         vtipne = ['nevim', 'vim']
         await ctx.send(f'{random.choice(vtipne)}')
+
+    # Link command
+    @commands.command()
+    async def link(self, ctx: commands.Context):
+        await ctx.send(f'google.com')
 
 bot = Bot()
 bot.run()
